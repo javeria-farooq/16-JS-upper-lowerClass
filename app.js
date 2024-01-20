9da69d1a72908e847b21b9f4a9f981073a4a0a82
javeria-farooq <gettingeducation6@gmail.com>
2024-01-20 15:57:12 +0500

    16 JS Assignment

---
+++ app.js
@@ -0,0 +1,54 @@
+// ab hmn kia krna h jse hmne phle kia city wla prompt bnaya th jsmn phle prompt mn user apna city insert krta h phr hm
+// alert mn show krwte th k city found h ya nh. t ab ismn kia hta h k agr user kbhi capital or small letter mn mistake
+// krde t user ko not found show na ho qk spelling sahi h bs srf capital or small mn mstake h t hm wo mstke khud hi thk
+// krdnge. or hm first letter ko capital mn rkhte hn bqi k small letters mn hte hn. iske lye uppercase or lowercase use
+// hta h.
+
+// let city = 'kArAcHi'
+// document.write(city.toUpperCase())
+// document.write(city.slice(0,2))
+
+// ab user se hmne input lia city name ka t usne karachi small mn dia or hmari array mn karachi ka k capital h ab is
+// wjah se user ko output t not found mle ga qk wo array t isko yhi consider krega k ye hmari array mn nh h. islye ab
+// user ko smbhlna h user chahe k small krde chahe capital krde kuch bh krde lkn user k input pe kaam hmn krna h.
+
+// let arr = ['Karachi', 'Islamabad', 'Peshawar', 'Quetta']
+// let match = false
+
+// let pro = prompt('enter city')
+// let pro1 = pro.slice(0,1).toUpperCase()
+// let pro2 = pro.slice(1).toLowerCase()
+// let res = pro1 + pro2
+
+// for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] == res) {
+//         match = true
+//         alert('City found')
+//         document.write(res)
+//         break
+//     }
+// }
+
+// if(match == false) {
+//     alert('City not found')
+// }
+
+// let arr = ['Karachi', 'Islamabad', 'Peshawar', 'Quetta']
+// let pro = prompt('Enter your city name')
+// let pro1 = pro.slice(0,1).toUpperCase()
+// let pro2 = pro.slice(1).toLowerCase()
+// let res = pro1 + pro2
+// let match = false
+
+// for(let i = 0; i < arr.length; i++) {
+//     if(res == arr[i]) {
+//         match = true
+//         alert('City found')
+//         document.write(res)
+//         break
+//     }
+// }
+
+// if(match == false) {
+//     alert('City not found')
+// }
